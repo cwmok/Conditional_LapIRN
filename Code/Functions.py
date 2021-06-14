6e9bc596fec73fc4a5ea81483c6d277921b08eee
cwmok <cwmokab@connect.ust.hk>
2021-06-14 21:22:13 +0800

    1st complete version

---
+++ Code/Functions.py
@@ -0,0 +1,216 @@
+import SimpleITK as sitk
+import numpy as np
+import torch.utils.data as Data
+import nibabel as nib
+import torch
+import glob
+import itertools
+
+
+def generate_grid(imgshape):
+    x = np.arange(imgshape[0])
+    y = np.arange(imgshape[1])
+    z = np.arange(imgshape[2])
+    grid = np.rollaxis(np.array(np.meshgrid(z, y, x)), 0, 4)
+    grid = np.swapaxes(grid,0,2)
+    grid = np.swapaxes(grid,1,2)
+    return grid
+
+# (grid[0, :, :, :, 0] - (size_tensor[3] / 2)) / size_tensor[3] * 2
+def generate_grid_unit(imgshape):
+    x = (np.arange(imgshape[0]) - ((imgshape[0]-1)/2)) / (imgshape[0]-1) * 2
+    y = (np.arange(imgshape[1]) - ((imgshape[1]-1)/2)) / (imgshape[1]-1) * 2
+    z = (np.arange(imgshape[2]) - ((imgshape[2]-1)/2)) / (imgshape[2]-1) * 2
+    grid = np.rollaxis(np.array(np.meshgrid(z, y, x)), 0, 4)
+    grid = np.swapaxes(grid,0,2)
+    grid = np.swapaxes(grid,1,2)
+    return grid
+
+
+def transform_unit_flow_to_flow(flow):
+    x, y, z, _ = flow.shape
+    flow[:, :, :, 0] = flow[:, :, :, 0] * z
+    flow[:, :, :, 1] = flow[:, :, :, 1] * y
+    flow[:, :, :, 2] = flow[:, :, :, 2] * x
+
+    return flow
+
+
+def transform_unit_flow_to_flow_cuda(flow):
+    b, x, y, z, c = flow.shape
+    flow[:, :, :, :, 0] = flow[:, :, :, :, 0] * z
+    flow[:, :, :, :, 1] = flow[:, :, :, :, 1] * y
+    flow[:, :, :, :, 2] = flow[:, :, :, :, 2] * x
+
+    return flow
+
+def load_4D(name):
+    # X = sitk.GetArrayFromImage(sitk.ReadImage(name, sitk.sitkFloat32 ))
+    # X = np.reshape(X, (1,)+ X.shape)
+    X = nib.load(name)
+    X = X.get_fdata()
+    X = np.reshape(X, (1,) + X.shape)
+    return X
+
+
+def load_5D(name):
+    # X = sitk.GetArrayFromImage(sitk.ReadImage(name, sitk.sitkFloat32 ))
+    X = fixed_nii = nib.load(name)
+    X = X.get_fdata()
+    X = np.reshape(X, (1,)+(1,)+ X.shape)
+    return X
+
+def imgnorm(img):
+    max_v = np.max(img)
+    min_v = np.min(img)
+    norm_img = (img - min_v) / (max_v - min_v)
+    return norm_img
+
+def save_img(I_img,savename):
+    # I2 = sitk.GetImageFromArray(I_img,isVector=False)
+    # sitk.WriteImage(I2,savename)
+    affine = np.diag([1, 1, 1, 1])
+    new_img = nib.nifti1.Nifti1Image(I_img, affine, header=None)
+    # save_path = os.path.join(output_path, file_index + "_wrapped_norm.nii")
+    nib.save(new_img, savename)
+
+
+def save_img_nii(I_img,savename):
+    # I2 = sitk.GetImageFromArray(I_img,isVector=False)
+    # sitk.WriteImage(I2,savename)
+    affine = np.diag([1, 1, 1, 1])
+    new_img = nib.nifti1.Nifti1Image(I_img, affine, header=None)
+    # save_path = os.path.join(output_path, savename)
+    nib.save(new_img, savename)
+
+
+def save_flow(I_img,savename):
+    # I2 = sitk.GetImageFromArray(I_img,isVector=True)
+    # sitk.WriteImage(I2,savename)
+    affine = np.diag([1, 1, 1, 1])
+    new_img = nib.nifti1.Nifti1Image(I_img, affine, header=None)
+    nib.save(new_img, savename)
+
+
+class Dataset(Data.Dataset):
+  'Characterizes a dataset for PyTorch'
+  def __init__(self, names,iterations,norm=False):
+        'Initialization'
+        self.names = names
+        self.norm = norm
+        self.iterations = iterations
+  def __len__(self):
+        'Denotes the total number of samples'
+        return self.iterations
+
+  def __getitem__(self, step):
+        'Generates one sample of data'
+        # Select sample
+        index_pair = np.random.permutation(len(self.names)) [0:2]
+        img_A = load_4D(self.names[index_pair[0]])
+        img_B = load_4D(self.names[index_pair[1]])
+        if self.norm:
+            return torch.from_numpy(imgnorm(img_A)).float(), torch.from_numpy(imgnorm(img_B)).float()
+        else:
+            return torch.from_numpy(img_A).float(), torch.from_numpy(img_B).float()
+
+
+class Dataset_epoch(Data.Dataset):
+  'Characterizes a dataset for PyTorch'
+  def __init__(self, names, norm=False):
+        'Initialization'
+        self.names = names
+        self.norm = norm
+        self.index_pair = list(itertools.permutations(names, 2))
+
+  def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.index_pair)
+
+  def __getitem__(self, step):
+        'Generates one sample of data'
+        # Select sample
+        img_A = load_4D(self.index_pair[step][0])
+        img_B = load_4D(self.index_pair[step][1])
+
+        # print(self.index_pair[step][0])
+        # print(self.index_pair[step][1])
+
+        if self.norm:
+            return torch.from_numpy(imgnorm(img_A)).float(), torch.from_numpy(imgnorm(img_B)).float()
+        else:
+            return torch.from_numpy(img_A).float(), torch.from_numpy(img_B).float()
+
+
+class Predict_dataset(Data.Dataset):
+    def __init__(self, fixed_list, move_list, fixed_label_list, move_label_list, norm=False):
+        super(Predict_dataset, self).__init__()
+        self.fixed_list = fixed_list
+        self.move_list = move_list
+        self.fixed_label_list = fixed_label_list
+        self.move_label_list = move_label_list
+        self.norm = norm
+
+    def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.move_list)
+
+    def __getitem__(self, index):
+        fixed_img = load_4D(self.fixed_list)
+        moved_img = load_4D(self.move_list[index])
+        fixed_label = load_4D(self.fixed_label_list)
+        moved_label = load_4D(self.move_label_list[index])
+
+        if self.norm:
+            fixed_img = imgnorm(fixed_img)
+            moved_img = imgnorm(moved_img)
+
+        fixed_img = torch.from_numpy(fixed_img)
+        moved_img = torch.from_numpy(moved_img)
+        fixed_label = torch.from_numpy(fixed_label)
+        moved_label = torch.from_numpy(moved_label)
+
+        if self.norm:
+            output = {'fixed': fixed_img.float(), 'move': moved_img.float(),
+                      'fixed_label': fixed_label.float(), 'move_label': moved_label.float(), 'index': index}
+            return output
+        else:
+            output = {'fixed': fixed_img.float(), 'move': moved_img.float(),
+                      'fixed_label': fixed_label.float(), 'move_label': moved_label.float(), 'index': index}
+            return output
+
+
+if __name__ == '__main__':
+    # datapath = '/home/wing/Desktop/registration/miccai2019/data_and_aseg/crop_min_max/norm'
+    # names = sorted(glob.glob(datapath + '/*.nii'))[0:255]
+    # dataset = Dataset_epoch(names, False)
+    # training_generator = Data.DataLoader(Dataset_epoch(names, norm=False), batch_size=1,
+    #                                      shuffle=False, num_workers=2)
+    # for X, Y in training_generator:
+    #     print("---")
+    # # 64770 pairs
+    # for i in range(0, dataset.__len__()):
+    #     print(i, dataset.__len__())
+    #     dataset.__getitem__(i)
+    #     print("---")
+
+    # imgshape = (5, 6, 7)
+    # x = np.arange(imgshape[0])
+    # y = np.arange(imgshape[1])
+    # z = np.arange(imgshape[2])
+    # grid = np.array(np.meshgrid(z, y, x)) #(3, 6, 7, 5)
+    # grid = np.rollaxis(grid, 0, 4)# (6, 7, 5, 3)
+    # grid = np.swapaxes(grid, 0, 2)# (5, 7, 6, 3)
+    # grid = np.swapaxes(grid, 1, 2)# (5, 6, 7, 3)
+
+    grid = generate_grid_unit((5, 6, 7))
+
+    print(grid[:, :, :, 0].min(), grid[:, :, :, 0].max()) # -1, 1
+    print(grid[:, :, :, 1].min(), grid[:, :, :, 1].max()) # -1, 1
+    print(grid[:, :, :, 2].min(), grid[:, :, :, 2].max()) # -1, 1
+
+    grid = generate_grid((5, 6, 7))
+    # print(grid[:, :, :, 0]) # 0-6
+    # print(grid[:, :, :, 1]) # 0-5
+    # print(grid[:, :, :, 2]) # 0-4
+    print("done")
--- README.md
+++ README.md
@@ -2,4 +2,50 @@
 
 This is the official Pytorch implementation of "Conditional Deformable Image Registration with Convolutional Neural Network" (MICCAI 2021), written by Tony C. W. Mok and Albert C. S. Chung.
 
-(Coming soon)
+## Prerequisites
+- `Python 3.5.2+`
+- `Pytorch 1.3.0 - 1.7.1`
+- `NumPy`
+- `NiBabel`
+
+This code has been tested with `Pytorch 1.7.1` and NVIDIA TITAN RTX GPU.
+
+## Inference
+```
+python Test_cLapIRN.py
+```
+
+The regularization weight can be changed by appending `--reg_input {{normalized weight within [0,1]}}` argument to the inference script. For example,
+```
+python Test_cLapIRN.py --reg_input 0.4
+```
+is equivalent to output the solution with regularization weight set to 4.
+
+## Train your own model
+Step 1: Replace `/PATH/TO/YOUR/DATA` with the path of your training data. You may also need to implement your own data generator (`Dataset_epoch` in `Functions.py`).
+
+Step 2: Change the `imgshape` variable in `Train_cLapIRN.py` to match the resolution of your data.
+
+Step 3: `python Train_cLapIRN.py` to train the model. Remember the data should be normalized within [0,1]. Otherwise, set `norm=True` in the provided data loader.
+
+(Optional): Implement the custome validation code in line 368 at `Train_cLapIRN.py`. 
+
+## Scalability/Out of memory error
+1. You may adjust the size of the model by manipulating the argument `--start_channel` in `Train_cLapIRN.py` and `Test_cLapIRN.py`
+
+2. You may adjust the number of conditional image registration module in `resblock_seq` function (at `Functions.py`). 
+
+
+## Publication
+If you find this repository useful, please cite:
+- **Conditional Deformable Image Registration with Convolutional Neural Network**  
+[Tony C. W. Mok](https://cwmok.github.io/ "Tony C. W. Mok"), Albert C. S. Chung  
+MICCAI 2021. [arXiv (coming soon)]
+
+
+## Acknowledgment
+Some codes in this repository are modified from [IC-Net](https://github.com/zhangjun001/ICNet) and [VoxelMorph](https://github.com/voxelmorph/voxelmorph).
+
+
+###### Keywords
+Keywords: Conditional Image registration, Controllable Regularization, Deformable Image Registration
